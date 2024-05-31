d9694048073c33cf53c7b3a5e8cbedf7aad86dcb
xsm3z <xsm.zhang@gmail.com>
2024-05-31 14:44:19 -0400

    2nd commit, completed step 1-3

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 /*-------------------------------- Constants --------------------------------*/
 
+const squareElements = document.querySelectorAll('.sqr');
+const messageElement = document.querySelector('#message');
+
+console.log(squareElements);
+console.log(messageElement);
+
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board = [ '', '', '', '', '', '', '', '', '' ]; 
+let turn = 'x';
+let winner = false;
+let tie = false; 
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -12,6 +21,15 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+const init = () => {
+  console.log('Game has started')
+  render()
+}
 
+init()
+
+const render = () => {
+
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
