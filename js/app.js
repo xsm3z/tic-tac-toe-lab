b7e895bacb8d20d330cc80c9a796f6763b59c16e
xsm3z <xsm.zhang@gmail.com>
2024-05-31 15:25:51 -0400

    3rd commit, step 4-5 complete

--- js/app.js
+++ js/app.js
@@ -1,35 +1,64 @@
 /*-------------------------------- Constants --------------------------------*/
 
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+
 const squareElements = document.querySelectorAll('.sqr');
 const messageElement = document.querySelector('#message');
 
 console.log(squareElements);
 console.log(messageElement);
 
+const winningCombos = [
+  [ 0, 1, 2 ],
+  [ 3, 4, 5 ],
+  [ 6, 7, 8 ],
+  [ 0, 3, 6 ],
+  [ 1, 4, 7 ],
+  [ 2, 5, 8 ],
+  [ 0, 4, 8 ],
+  [ 6, 4, 2 ]
+]
 
-
-/*---------------------------- Variables (state) ----------------------------*/
-
-let board = [ '', '', '', '', '', '', '', '', '' ]; 
+let board = [
+  '', '', '',
+  '', '', '',
+  '', '', ''
+]; 
 let turn = 'x';
 let winner = false;
 let tie = false; 
 
-/*------------------------ Cached Element References ------------------------*/
-
-
-
-/*-------------------------------- Functions --------------------------------*/
-
 const init = () => {
-  console.log('Game has started')
-  render()
-}
+  console.log('Game has started');
+  render();
+};
 
-init()
+window.onload = init;
+
+const updateBoard = () => {
+  board.forEach((value, idx) => {
+    const square = squareElements[idx];
+    square.textContent = value;
+  });
+};
+
+const updateMessage = () => {
+  if( winner === false && tie === false ) {
+    messageElement.textContent = `It's player ${turn}'s turn`;
+  } else if ( winner ===  false && tie === true ) {
+    messageElement.textContent = "Cat's game!"
+  } else {
+    messageElement.textContent = `Player ${turn} has won!`
+  }
+};
 
 const render = () => {
-
-}
+  updateBoard();
+  updateMessage();
+};
 
 /*----------------------------- Event Listeners -----------------------------*/
